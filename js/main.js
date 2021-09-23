2ece351f2774e04d53af9d509551e4313e5abe2d
unknown <axel.duval@gmail.com>
2021-09-23 16:58:34 +0200

    Ajout de la fonctionnalité de comparaison des lettres

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ function splitChoice(words) {
 }
 // Create a copy of the word and hide it
 function hideChoice() {
-    return computerChoice.map(x => " _ ");
+    return computerChoice.map(x => " _ ").join(" ");
 }
 // Store the random choice splited
 let computerChoice = splitChoice(words);
@@ -22,11 +22,10 @@ let computerChoice = splitChoice(words);
 // Store the hidden word
 let hidenText = hideChoice();
 
-// Compare the user choice and the word
- 
-
-
-
+// Compare the user choice presence in the word
+ function checkLetter(userChoice) {
+   return computerChoice.includes(userChoice);
+ };
 
 
 // Ask & Store the user choice
@@ -34,8 +33,11 @@ do {
 userChoice = prompt("Choisissez une lettre !");
 } while(userChoice.length !== 1)
 
+let wordPresence = checkLetter(userChoice);
 
 
+
+console.log(wordPresence);
 console.log(computerChoice);
 console.log(hidenText);
 console.log(userChoice);
