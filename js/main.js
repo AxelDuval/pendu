0f2b03017847b6b03b5de9bb8fd5700f4738167b
AxelDuval <axel.duval@gmail.com>
2021-09-24 14:58:13 +0200

    Ajout des conditions de Win/Loose - Version de base du jeu COMPLETE

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 // VARIABLES
-// Words to guess
 const words = ["chien", "poisson", "ascenseur", "voiture", "souris"];
 let userChoice;
 let healthPoints = 7;
 let letterPosition;
+let wordFound;
+let computerChoice = splitChoice(words);
+let hiddenText = hideChoice();
 
 // FUNCTIONS
 //Computer random choice 
@@ -17,77 +19,40 @@ function splitChoice(words) {
 // Create a copy of the word and hide it
 function hideChoice() {
     return computerChoice.map(x => " _ "); 
-    // .join(" ")
 }
-// Store the random choice splited
-let computerChoice = splitChoice(words);
+// Compare the user choice presence in the word
+ function checkLetter() {
+   if (computerChoice.includes(userChoice)){
+    console.log("présent") ;
+    return computerChoice.indexOf(userChoice);
+   }
+   else {   
+       return false;
+   }
+ }
 
-// Store the hidden word
-let hiddenText = hideChoice();
-
-// Get the number of letters in the random word
-// remainLetters = hiddenText.length;
-
-
-// // Compare the user choice presence in the word
-//  function checkLetter() {
-//    if (computerChoice.includes(userChoice)){
-//     console.log("présent") ;
-//     return computerChoice.indexOf(userChoice);
-//    }
-//    else {   
-//        return false;
-//    }
-//  }
-
-
-// The game loop
-
-    
-// Get the player choice
-      
-while(hiddenText.includes(" _ ") || healthPoints === 0 || guess.length !== 1){
-    userChoice = prompt("Essais restants : " + healthPoints  + "\n\n " +  "Mot à deviner  : "  + hiddenText.join(" ") + "\n\n Choisissez une lettre !");
-
-    for (let i = 0; i < computerChoice.length; i++) {                
+// GAME LOOP
+// Get the player choice    
+while(hiddenText.includes(" _ ") || healthPoints === 0){
+   do { userChoice = prompt("Essais restants : " + healthPoints  + "\n\n " +  "Mot à deviner  : "  + hiddenText.join(" ") + "\n\n Choisissez une lettre !");
+      } while (userChoice.length !== 1);
+    // Compare to the random word 
+    for (let i = 0; i < computerChoice.length; i++) {           
     if (computerChoice[i] === userChoice) {
-    hiddenText[i] = userChoice ;
+        hiddenText[i] = userChoice;
+        }   
     }
-
-    
-}}
-
-//     if(letterPosition != false) {
-//         letterPosition = checkLetter(userChoice);
-//         hiddenText[letterPosition] = userChoice;
-//     }
-
-//     else {
-//     // Update the game state with the guess
-//     healthPoints--;
-// }}
-    
-    
-    
-    // The end of the game loop
-    
-
-    // if (letterPosition != false){
-    //     letterPosition = checkLetter(userChoice);
-    //     hiddenText[letterPosition] = userChoice;
-    // }
-    //     else {
-    //         healthPoints--;
-    //     }
-
-
-
-// while (healthPoints !=0 && hiddenText.includes(" _ ")) {
-
-   
-
-
-console.log("choix de l'ordinateur : " + computerChoice);
-console.log("choix utilisateur : " + userChoice);
-console.log("Index de la lettre : " + checkLetter());
-// alert("Texte caché " + hiddenText);
+    // lose health
+    if (checkLetter() === false) {        
+        healthPoints--;
+    }
+    // lose game
+    if (healthPoints === 0){
+        alert("Perdu ! Le mot était : " + computerChoice.join("")); 
+        break;
+    }
+    // win game
+    else if (hiddenText.includes(" _ ") === false) {  
+        alert("Gagné ! Vous avez trouvé le mot : " + computerChoice.join(""));
+    }
+}
